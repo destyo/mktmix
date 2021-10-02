adc5e486bbd14373cc75717d78dcb2c2e7a86a00
Your Name <you@example.com>
2021-10-02 12:36:48 +0200

    0.0.1

--- asessment.R
+++ asessment.R
@@ -1,5 +1,11 @@
 #
 # R programming asessment. Marketing Mix Model. 
 #
+library(dplyr)
+library(ggplot2)
+library(janitor)
 
-
+#1----
+df_mmm <- read.csv("mktmix.csv")
+df_mmm <- clean_names(df_mmm)
+glimpse(df_mmm)
